8a4f017e2706f490ea805f3bfee8abae0ca94cf4
Beckam01 <habtamubeckam121@gmail.com>
2025-07-12 16:10:15 +0300

    Add functions for word detection and password checking, along with HTML structure

---
+++ algorithm/practice/pract.js
@@ -0,0 +1,139 @@
+// function abebe() {
+//     console.log("Abeb");
+//     console.log("abebe beso bela");
+//     var num1 = 23;
+//     var num2 = 43;
+//     console.log(num1 + num2);
+// }
+// abebe();
+// for (i = 1; i <= 10; i++) {
+//     console.log("hello");
+// }
+
+
+// var studentGrade = [12, 34, 54, 76, 89, 98, 54,];
+// var pass = 50;
+
+// for (i = 0; i < studentGrade.length; i++) {
+//     console.log(studentGrade[i]);
+// }
+
+
+
+
+// function add() {
+//     console.log(13 * 10);
+//     console.log(13 + 10);
+//     console.log(13 - 10);
+//     console.log(13 / 10);
+// }
+// add();
+
+
+// var student = [21,43,10,6,12,33,];
+// var pass = 50;
+
+
+// x is parameter yeblal
+//
+
+// function kebed (x){
+//  console.log("kebe selam new");
+// console.log(x);
+
+// }
+// kebed("chala chube chebte");
+
+
+// function laptoppricecalculator(laptopprice, price, discount) {
+//     let tax = (price * 6) / 100;
+//     let total = price - discount - tax;
+//     console.log(laptopprice + "laptop price is 5" + total);
+
+// }
+// laptoppricecalculator("HP", 1000, 50);
+// laptoppricecalculator("dell", 1123, 43);
+
+// function adder(x,y){
+//     var c;
+//     c = x + y;
+//     return c;
+// }
+
+// var adder = adder(8,9);
+// console.log(adder);
+
+// function adder(x, y) {
+//     console.log(x + y);
+
+//     return x + y;
+// }
+// console.log(adder(5, 7));
+
+
+// function average(num1, num2) {
+//     let result = adder(num1, num2) / 2;
+//     let sum = num1 + num2;
+//     //console.log(result);
+//     return [result, sum];
+// }
+// console.log(average(100, 20));
+
+
+// const addnumb = (num1, num2) => {
+//     return num1 + num2;
+
+// }
+// console.log(addnumb(59, 20));
+
+
+
+
+function passwordcheck() {
+    if (pass1 == pass2); {
+        console.log("password confirmed");
+    }
+
+    if (pass1 !== pass1) {
+        console.log("The password you entered is donot match ");
+
+    }
+
+}
+console.log("abeb1234", "Abebe1234");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function WKHA(x, y) {
+    let result = x + y;
+
+}
+
+
+
+
